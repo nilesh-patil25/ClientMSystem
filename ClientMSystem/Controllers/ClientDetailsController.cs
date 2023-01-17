059213f7baf749923dfbd5592be9a0cac3b24e77
Nilesh Patil <abhishekchidrawar@gmail.com>
2023-01-17 17:18:42 +0530

    first commit

---
+++ ClientMSystem/Controllers/ClientDetailsController.cs
@@ -0,0 +1,142 @@
+﻿using ClientMSystem.Data;
+using ClientMSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientMSystem.Controllers
+{
+    public class ClientDetailsController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public ClientDetailsController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                return View();
+            }
+            else
+            {
+                var result = context.clientDetails.Where(x => x.UserId == userId).ToList();
+                return View(result);
+            }
+            
+        }
+
+        //Create
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ClientDetail model)
+        {
+            try
+            {
+                var userId = HttpContext.Session.GetInt32("UserId");
+                 
+
+                if (ModelState.IsValid && userId.HasValue)
+                {
+                    var rec = new ClientDetail()
+                    {
+                        //Id = model.Id,
+                        //UserId = model.UserId,
+                        UserId = userId.Value,
+                        Name = model.Name,
+                        ClientName = model.ClientName,
+                        IssuedDate = model.IssuedDate,
+                        DomainName = model.DomainName,
+                        Technology = model.Technology,
+                        Assigned = model.Assigned,
+                    };
+                    context.clientDetails.Add(rec);
+                    context.SaveChanges();
+                    TempData["Message"] = "Sheet Updated Successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Error"] = "Enter All Details";
+                    return View(model);
+                }
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var rec = context.clientDetails.SingleOrDefault(e => e.Id == id);
+                context.clientDetails.Remove(rec);
+                context.SaveChanges();
+                TempData["Error"] = "Update Delete Successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var model = context.clientDetails.SingleOrDefault(e => e.Id == id);
+            var result = new ClientDetail()
+            {
+                UserId = model.UserId,
+                Name = model.Name,
+                ClientName = model.ClientName,
+                IssuedDate = model.IssuedDate,
+                DomainName = model.DomainName,
+                Technology = model.Technology,
+                Assigned = model.Assigned,
+
+            };
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(ClientDetail model)
+        {
+            try
+            {
+                var rec = new ClientDetail()
+                {
+                    Id = model.Id,
+                    UserId = model.UserId,
+                    Name = model.Name,
+                    ClientName = model.ClientName,
+                    IssuedDate = model.IssuedDate,
+                    DomainName = model.DomainName,
+                    Technology = model.Technology,
+                    Assigned = model.Assigned,
+
+                };
+                context.clientDetails.Update(rec);
+                context.SaveChanges();
+                TempData["Error"] = "Sheet Updated Successfully";
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+}
---
+++ ClientMSystem/Controllers/TaskSheetController.cs
@@ -0,0 +1,146 @@
+﻿using ClientMSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using ClientMSystem.Models;
+using IronPdf;
+
+namespace ClientMSystem.Controllers
+{
+    public class TaskSheetController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public TaskSheetController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            if (userId == null)
+            {
+                return View();
+            }
+            else
+            {
+                var result = context.timeSheets.ToList(); // to show thw details on view
+                return View(result);
+            }
+
+            
+        }
+
+        //Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TimeSheet model)
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            try
+            {
+                if (ModelState.IsValid && userId.HasValue)
+                {
+                    var res = new TimeSheet()
+                    {
+                        Date = model.Date,
+                        Module = model.Module,
+                        ExpectedTaskToCompleted = model.ExpectedTaskToCompleted,
+                        ExpectedHours = model.ExpectedHours,
+                        CompletedTasks = model.CompletedTasks,
+                        UnPlannedTask = model.UnPlannedTask,
+                        ActualHours = model.ActualHours,
+                        CommentsForAnyDealy = model.CommentsForAnyDealy,
+                        QuestionsActionsToBeAsked = model.QuestionsActionsToBeAsked,
+                    };
+                    context.timeSheets.Add(res);
+                    context.SaveChanges();
+                    TempData["error"] = "Sheet Updated Successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Error"] = "Enter All Details";
+                    return View(model);
+                }
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var rec = context.timeSheets.SingleOrDefault(e => e.Id == id);
+                context.timeSheets.Remove(rec);
+                context.SaveChanges();
+                TempData["Error"] = "Update Delete Successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var model = context.timeSheets.SingleOrDefault(e => e.Id == id);
+            var result = new TimeSheet()
+            {
+               
+                Date = model.Date,
+                Module = model.Module,
+                ExpectedTaskToCompleted = model.ExpectedTaskToCompleted,
+                ExpectedHours = model.ExpectedHours,
+                CompletedTasks = model.CompletedTasks,
+                UnPlannedTask = model.UnPlannedTask,
+                ActualHours = model.ActualHours,
+                CommentsForAnyDealy = model.CommentsForAnyDealy,
+                QuestionsActionsToBeAsked = model.QuestionsActionsToBeAsked,
+
+            };
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(TimeSheet model)
+        {
+            try
+            {
+                var rec = new TimeSheet()
+                {
+                    Id = model.Id,
+                    Date = model.Date,
+                    Module = model.Module,
+                    ExpectedTaskToCompleted = model.ExpectedTaskToCompleted,
+                    ExpectedHours = model.ExpectedHours,
+                    CompletedTasks = model.CompletedTasks,
+                    UnPlannedTask = model.UnPlannedTask,
+                    ActualHours = model.ActualHours,
+                    CommentsForAnyDealy = model.CommentsForAnyDealy,
+                    QuestionsActionsToBeAsked = model.QuestionsActionsToBeAsked,
+
+                };
+                context.timeSheets.Update(rec);
+                context.SaveChanges();
+                TempData["Error"] = "Sheet Updated Successfully";
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
